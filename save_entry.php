ea9af03c030565c7e2deeb33d50d45651d5dc621
hoffr <37904227+hoffr@users.noreply.github.com>
2023-07-28 05:27:45 -0400

    Update save_entry.php

--- save_entry.php
+++ save_entry.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $data = file_get_contents("php://input");
     $entry = json_decode($data, true);
