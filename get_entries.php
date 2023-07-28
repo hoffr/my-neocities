fcf9b6bb973831c57cfbe63c3781e946d64a275a
hoffr <37904227+hoffr@users.noreply.github.com>
2023-07-28 05:28:03 -0400

    Create get_entries.php

---
+++ get_entries.php
@@ -0,0 +1,15 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+if ($_SERVER["REQUEST_METHOD"] === "GET") {
+    // Check if the JSON file exists
+    if (file_exists("guestbook_entries.json")) {
+        // Get existing entries
+        $entries = json_decode(file_get_contents("guestbook_entries.json"), true);
+        echo json_encode($entries);
+    } else {
+        echo json_encode([]);
+    }
+}
+?>
